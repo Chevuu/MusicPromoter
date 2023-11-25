dd32b23d6d32394fa319672924cc7a55890121ff
Chevuu <vukjurisic@gmail.com>
2023-11-25 19:48:31 +0000

    finished first front-end draft

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Player from './components/Player';
 import './index.css';
 
 const App = () => {
-  const players = new Array(15).fill(null).map((_, index) => ({
+  const [playingId, setPlayingId] = useState(null);
+
+  const handlePlayRequest = (id) => {
+    if (playingId !== id) {
+      setPlayingId(id);
+    }
+  };
+
+  const players = new Array(16).fill(null).map((_, index) => ({
     id: index + 1,
-    title: `Sample Song ${index + 1}`,
+    title: `Song ${index + 1}`,
     songUrl: `song${index + 1}.mp3`,
     thumbnailUrl: `thumbnail${index + 1}.jpg`,
   }));
@@ -15,8 +23,11 @@ const App = () => {
       {players.map((player) => (
         <Player
           key={player.id}
+          id={player.id}
+          isPlaying={player.id === playingId}
+          onPlayRequest={handlePlayRequest}
           title={player.title}
-          sampleUrl={player.songUrl}
+          songUrl={player.songUrl}
           thumbnail={player.thumbnailUrl}
         />
       ))}
